f004c26e3faec53eaca480f10055d1f7ff08dc44
Simon Wahlin <simon.wahlin@advania.com>
2020-03-26 17:03:38 +0100

    configured subscription and tenant

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "azurerm" {
-  version = ">= 1.1.0"
+  version = "=1.36.0"
+  tenant_id = "ad74fd2e-a48f-454d-9d44-020b5180b88b"
+  subscription_id = "9c1421cd-fb59-4c35-94e5-c83e65a068ee"
 }
 
 resource "azurerm_resource_group" "rg" {
